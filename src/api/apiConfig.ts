95b34b6799ce94439fc39dd138d339d28d2080cc
batool2azzam <batoolazzam16@gmail.com>
2024-06-05 13:31:51 +0300

    move axios configurations to another file

---
+++ src/api/apiConfig.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const API_URL = "https://tarmeezacademy.com/api/v1";
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("auth_token");
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            console.error("Unauthorized access - possibly invalid token");
+            localStorage.removeItem("auth_token");
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
